e2a83073a12afa74d42e3612bd26e9c222913ccd
Abhishek kumar <kumarabhishek06437@gmail.com>
2026-08-08 00:26:44 +0530

    Time: 179 ms (78.08%) | Memory: 34.8 MB (77.59%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -0,0 +1,25 @@
+from collections import deque 
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        dq=deque()
+        ans =[]
+        n=len(nums)
+        for i in range(n):
+            while dq and dq[0]<=i-k:#Revome indices outside the current window 
+                dq.popleft()
+            while dq and nums[dq[-1]]<nums[i]:#Remove smaller elements from the back 
+                dq.pop()
+            dq.append(i)#starting adding answers once the first window is formed 
+
+            if i>=k-1:
+                ans.append(nums[dq[0]])#In dedue front elements always maximum 
+        return ans 
+
+       # n=len(nums)
+       # ans =[]
+       # for i in range(n-k+1):
+          #  for j in range(i,i+k):
+              #  if nums[j]>nums[i]:
+                #    nums[i]=nums[j]
+           # ans.append(nums[i])
+       # return ans 
