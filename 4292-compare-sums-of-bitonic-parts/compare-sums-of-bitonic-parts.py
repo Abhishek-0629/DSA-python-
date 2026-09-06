aeffc799a2a9d5efc80e6388c56c38d5de99c9e5
Abhishek kumar <kumarabhishek06437@gmail.com>
2026-09-06 17:51:50 +0530

    Time: 71 ms (14.75%) | Memory: 35.3 MB (11.52%) - LeetSync

--- 4292-compare-sums-of-bitonic-parts/compare-sums-of-bitonic-parts.py
+++ 4292-compare-sums-of-bitonic-parts/compare-sums-of-bitonic-parts.py
@@ -1,21 +1,21 @@
 class Solution:
     def compareBitonicSums(self, nums: list[int]) -> int:
-        peak = 0 
+        peak=0
         n=len(nums)
-        for i in range(n-1):
+        for i in range(n):
             if nums[i]>nums[i+1]:
                 peak=i
                 break
-        left = 0
-        right = 0
+        leftsum=0
+        rightsum=0
         for i in range(peak+1):
-            left+=nums[i]
+            leftsum+=nums[i]
         for i in range(peak,n):
-            right+=nums[i]
-
-        if left>right:
+            rightsum+=nums[i]
+        if leftsum>rightsum:
             return 0 
-        elif left<right:
+        elif leftsum<rightsum:
             return 1
         else:
             return -1 
+        
